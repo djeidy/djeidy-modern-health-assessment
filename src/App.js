b11a67a3a675fa7cc26ecf5549181a7af52ba52f
Mahamadou Djeidy Ba <mdjeidyba@atlassian.com>
2020-11-11 15:54:48 -0800

    complete task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import { messages as data } from './data.json';
+import {deduplicate, formatDate} from "./utils";
+import {Button, Col, Container, ListGroup, Row} from "react-bootstrap";
 
 function App() {
+  const [loadAmount, setAmount] = React.useState(5);
+  const [messages, setMessages] = React.useState(deduplicate(data, "uuid", "content"));
+
+  const deleteHandler = (id, content) => {
+    const newMessages = [...messages];
+    let toDelete = newMessages.findIndex(message => message.uuid === id && message.content === content)
+    newMessages.splice(toDelete, 1);
+    setMessages(newMessages);
+    setAmount(prevAmount => prevAmount - 1);
+  }
+  const loadMoreHandler = () => {
+    if(loadAmount + 5 < messages.length){
+      setAmount(prevAmount => prevAmount + 5);
+    } else if(loadAmount !== messages.length && loadAmount < messages.length){
+      setAmount(prevAmount => prevAmount + messages.length - loadAmount)
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ListGroup>
+        {
+          messages.sort((a, b) => {
+            return a.sentAt > b.sentAt ? -1 : b.sentAt > a.sentAt ? 1 : 0;
+          }).slice(0, loadAmount).map((message, index) => {
+            return (
+              <ListGroup.Item key={index}>
+                <Row>
+                  <Col sm={4}>
+                    {message.uuid}
+                  </Col>
+                  <Col sm={7}>
+                    {formatDate(message.sentAt)}
+                  </Col>
+                  <Col sm={1}>
+                    <Button variant={"danger"} type={"button"} onClick={()=> deleteHandler(message.uuid, message.content)}>
+                      X
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            )
+          })
+        }
+        <Row>
+          <Button variant={"primary"} className="btn btn-block" type={"button"} onClick={loadMoreHandler}>
+            LOAD MORE
+          </Button>
+        </Row>
+      </ListGroup>
+    </Container>
   );
 }
 
